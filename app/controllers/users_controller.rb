6592d0e0da9de09fbbf616646dbc1428ca84d89c
kalavhan <josue.brigido1@gmail.com>
2020-01-15 13:59:22 -0700

    Finish chapter 9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
       log_in @user
+      remember user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
